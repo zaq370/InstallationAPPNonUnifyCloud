11c30447e021c7f17f213143fca8139e925f9a7d
zaq370 <lulumi.chao@gmail.com>
2026-02-09 15:36:07 +0800

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InstallationAPPNonUnify.MvcApplication" Language="C#" %>
---
+++ Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using InstallationAPPNonUnify.App_Start;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace InstallationAPPNonUnify
+{
+    // 注意: 如需啟用 IIS6 或 IIS7 傳統模式的說明，
+    // 請造訪 http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            ModelBinders.Binders.DefaultBinder = new ViewModelBinder();
+            CustomeServices.ServiceInitiate();
+
+        }
+    }
+}
